b96c2d09ef329f14b934ddf768c0481623382a0a
Gunil <gunil.na1@gmail.com>
2020-10-15 16:04:39 +0200

    changed code a little bit in shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -25,8 +25,9 @@ class ShopPage extends React.Component {
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
+
     // whenever the collectionRef updates or when it runs for the first time
-    collectionRef.onSnapshot(async (snapshot) => {
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
