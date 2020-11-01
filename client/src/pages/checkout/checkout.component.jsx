ecd32e9c30833dd129fb66ccf9b8a92796dfa807
Gunil <gunil.na1@gmail.com>
2020-11-01 18:02:47 +0100

    added test code for pages

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -16,7 +16,7 @@ import {
   WarningContainer,
 } from "./checkout.styles";
 
-const CheckoutPage = ({ cartItems, total }) => (
+export const CheckoutPage = ({ cartItems, total }) => (
   <CheckoutPageContainer>
     <CheckoutHeaderContainer>
       <HeaderBlockContainer>
