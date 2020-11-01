812326af10104fd79460514491d2e5cfcafce252
Gunil <gunil.na1@gmail.com>
2020-11-01 16:52:30 +0100

    added test code for cart and collection components

--- client/src/components/checkout-item/checkout-item.styles.jsx
+++ client/src/components/checkout-item/checkout-item.styles.jsx
@@ -17,6 +17,7 @@ export const CheckoutItemContainer = styled.div`
 export const ImageContainer = styled.div`
   width: 23%;
   padding-right: 15px;
+
   img {
     width: 100%;
     height: 100%;
@@ -33,15 +34,21 @@ export const TextContainer = styled.span`
 
 export const QuantityContainer = styled(TextContainer)`
   display: flex;
+
   span {
     margin: 0 10px;
   }
+
   div {
     cursor: pointer;
   }
 `;
 
+QuantityContainer.displayName = "QuantityContainer";
+
 export const RemoveButtonContainer = styled.div`
   padding-left: 12px;
   cursor: pointer;
 `;
+
+RemoveButtonContainer.displayName = "RemoveButtonContainer";
